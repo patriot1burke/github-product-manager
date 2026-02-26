67a211e18e1f14a1afcfccfe43a1935aeef16b15
Bill Burke <bburke@redhat.com>
2026-02-26 10:36:14 -0500

    doc

--- github-ingester/src/main/java/io/quarkiverse/graphql/client/ArgsOnly.java
+++ github-ingester/src/main/java/io/quarkiverse/graphql/client/ArgsOnly.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * When this annotation is present, the method does not define a field.  Arguments to the method will assign field variables
+ * to the current field namespace.
+ * 
+ * This is useful in situations where you've mapped out query field variables, but want to request
+ * a different graph for the query.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface ArgsOnly {
